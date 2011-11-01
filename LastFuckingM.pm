f33deaae0849597290066ed5607280cbd5e29eac
Steve <nostrasteve@gmail.com>
2011-11-02 01:59:13 +1300

    know which methods require auth, post, signing

--- LastFuckingM.pm
+++ LastFuckingM.pm
@@ -7,16 +7,155 @@ use LWP::UserAgent;
 use Digest::MD5 'md5_hex';
 use URI;
 
-my $url = 'http://ws.audioscrobbler.com/2.0/';
-my $api_key = 'dfab9b1c7357c55028c84b9a8fb68880';
-my $secret = 'd004c86dcfa8ef4c3977b04f558535f2';
-my $sk_symlink = "$ENV{HOME}/.last-fucking-m-sessionkey";
-my $sk = readlink($sk_symlink);
-my $ua = new LWP::UserAgent(agent => "LastFuckingM/-666");
+our $url = 'http://ws.audioscrobbler.com/2.0/';
+our $api_key = 'dfab9b1c7357c55028c84b9a8fb68880';
+our $secret = 'd004c86dcfa8ef4c3977b04f558535f2';
+our $ua = new LWP::UserAgent(agent => "LastFuckingM/-666");
 
-# TODO crawl api docs
-my %post_methods = map { $_ => 1 } qw{track.scrobble};
-my %sk_methods = map { $_ => 1 } qw{track.scrobble};
+our $sk_symlink = "$ENV{HOME}/.last-fucking-m-sessionkey";
+sub load_key {
+    return readlink($sk_symlink);
+}
+sub save_key {
+    my $key = shift;
+    symlink($key, $sk_symlink);
+}
+our $sk = load_key();
+
+our $methods = {
+    'album.addTags' => { auth => 1, post => 1, signed => 1 },
+    'album.getBuylinks' => {  },
+    'album.getInfo' => {  },
+    'album.getShouts' => {  },
+    'album.getTags' => { auth => 1, signed => 1 },
+    'album.getTopTags' => {  },
+    'album.removeTag' => { auth => 1, post => 1, signed => 1 },
+    'album.search' => {  },
+    'album.share' => { auth => 1, post => 1, signed => 1 },
+    'artist.addTags' => { auth => 1, post => 1, signed => 1 },
+    'artist.getCorrection' => {  },
+    'artist.getEvents' => {  },
+    'artist.getImages' => {  },
+    'artist.getInfo' => {  },
+    'artist.getPastEvents' => {  },
+    'artist.getPodcast' => {  },
+    'artist.getShouts' => {  },
+    'artist.getSimilar' => {  },
+    'artist.getTags' => { auth => 1, signed => 1 },
+    'artist.getTopAlbums' => {  },
+    'artist.getTopFans' => {  },
+    'artist.getTopTags' => {  },
+    'artist.getTopTracks' => {  },
+    'artist.removeTag' => { auth => 1, post => 1, signed => 1 },
+    'artist.search' => {  },
+    'artist.share' => { auth => 1, post => 1, signed => 1 },
+    'artist.shout' => { auth => 1, post => 1, signed => 1 },
+    'auth.getMobileSession' => { signed => 1 },
+    'auth.getSession' => { signed => 1 },
+    'auth.getToken' => { signed => 1 },
+    'chart.getHypedArtists' => {  },
+    'chart.getHypedTracks' => {  },
+    'chart.getLovedTracks' => {  },
+    'chart.getTopArtists' => {  },
+    'chart.getTopTags' => {  },
+    'chart.getTopTracks' => {  },
+    'event.attend' => { auth => 1, post => 1, signed => 1 },
+    'event.getAttendees' => {  },
+    'event.getInfo' => {  },
+    'event.getShouts' => {  },
+    'event.share' => { auth => 1, post => 1, signed => 1 },
+    'event.shout' => { auth => 1, post => 1, signed => 1 },
+    'geo.getEvents' => {  },
+    'geo.getMetroArtistChart' => {  },
+    'geo.getMetroHypeArtistChart' => {  },
+    'geo.getMetroHypeTrackChart' => {  },
+    'geo.getMetroTrackChart' => {  },
+    'geo.getMetroUniqueArtistChart' => {  },
+    'geo.getMetroUniqueTrackChart' => {  },
+    'geo.getMetroWeeklyChartlist' => {  },
+    'geo.getMetros' => {  },
+    'geo.getTopArtists' => {  },
+    'geo.getTopTracks' => {  },
+    'group.getHype' => {  },
+    'group.getMembers' => {  },
+    'group.getWeeklyAlbumChart' => {  },
+    'group.getWeeklyArtistChart' => {  },
+    'group.getWeeklyChartList' => {  },
+    'group.getWeeklyTrackChart' => {  },
+    'library.addAlbum' => { auth => 1, post => 1, signed => 1 },
+    'library.addArtist' => { auth => 1, post => 1, signed => 1 },
+    'library.addTrack' => { auth => 1, post => 1, signed => 1 },
+    'library.getAlbums' => {  },
+    'library.getArtists' => {  },
+    'library.getTracks' => {  },
+    'library.removeAlbum' => { auth => 1, post => 1, signed => 1 },
+    'library.removeArtist' => { auth => 1, post => 1, signed => 1 },
+    'library.removeScrobble' => { auth => 1, post => 1, signed => 1 },
+    'library.removeTrack' => { auth => 1, post => 1, signed => 1 },
+    'playlist.addTrack' => { auth => 1, post => 1, signed => 1 },
+    'playlist.create' => { auth => 1, post => 1, signed => 1 },
+    'radio.getPlaylist' => { auth => 1, signed => 1 },
+    'radio.search' => {  },
+    'radio.tune' => { auth => 1, post => 1, signed => 1 },
+    'tag.getInfo' => {  },
+    'tag.getSimilar' => {  },
+    'tag.getTopAlbums' => {  },
+    'tag.getTopArtists' => {  },
+    'tag.getTopTags' => {  },
+    'tag.getTopTracks' => {  },
+    'tag.getWeeklyArtistChart' => {  },
+    'tag.getWeeklyChartList' => {  },
+    'tag.search' => {  },
+    'tasteometer.compare' => {  },
+    'tasteometer.compareGroup' => {  },
+    'track.addTags' => { auth => 1, post => 1, signed => 1 },
+    'track.ban' => { auth => 1, post => 1, signed => 1 },
+    'track.getBuylinks' => {  },
+    'track.getCorrection' => {  },
+    'track.getFingerprintMetadata' => {  },
+    'track.getInfo' => {  },
+    'track.getShouts' => {  },
+    'track.getSimilar' => {  },
+    'track.getTags' => { auth => 1, signed => 1 },
+    'track.getTopFans' => {  },
+    'track.getTopTags' => {  },
+    'track.love' => { auth => 1, post => 1, signed => 1 },
+    'track.removeTag' => { auth => 1, post => 1, signed => 1 },
+    'track.scrobble' => { auth => 1, post => 1, signed => 1 },
+    'track.search' => {  },
+    'track.share' => { auth => 1, post => 1, signed => 1 },
+    'track.unban' => { auth => 1, post => 1, signed => 1 },
+    'track.unlove' => { auth => 1, post => 1, signed => 1 },
+    'track.updateNowPlaying' => { auth => 1, post => 1, signed => 1 },
+    'user.getArtistTracks' => {  },
+    'user.getBannedTracks' => {  },
+    'user.getEvents' => {  },
+    'user.getFriends' => {  },
+    'user.getInfo' => {  },
+    'user.getLovedTracks' => {  },
+    'user.getNeighbours' => {  },
+    'user.getNewReleases' => {  },
+    'user.getPastEvents' => {  },
+    'user.getPersonalTags' => {  },
+    'user.getPlaylists' => {  },
+    'user.getRecentStations' => { auth => 1, signed => 1 },
+    'user.getRecentTracks' => {  },
+    'user.getRecommendedArtists' => { auth => 1, signed => 1 },
+    'user.getRecommendedEvents' => { auth => 1, signed => 1 },
+    'user.getShouts' => {  },
+    'user.getTopAlbums' => {  },
+    'user.getTopArtists' => {  },
+    'user.getTopTags' => {  },
+    'user.getTopTracks' => {  },
+    'user.getWeeklyAlbumChart' => {  },
+    'user.getWeeklyArtistChart' => {  },
+    'user.getWeeklyChartList' => {  },
+    'user.getWeeklyTrackChart' => {  },
+    'user.shout' => { auth => 1, post => 1, signed => 1 },
+    'venue.getEvents' => {  },
+    'venue.getPastEvents' => {  },
+    'venue.search' => {  },
+};
 
 sub req {
     my ($method, %params) = @_;
@@ -27,17 +166,19 @@ sub req {
 
     sign(\%params);
 
-    my $res = $post_methods{$method} ?
-        $ua->post($url, Content => \%params)
-        : $ua->get(do {
-            my $uri = URI->new($url);
-            $uri->query_form(%params);
-            $uri });
+    my $res;
+    if ($methods->{$method}->{post}) {
+        $res = $ua->post($url, Content => \%params);
+    }
+    else {
+        my $uri = URI->new($url);
+        $uri->query_form(%params);
+        $res = $ua->get($uri);
+    }
 
     my $content = $res->decoded_content;
     unless ($res->is_success && $content =~ /<lfm status="ok">/) {
-        use YAML::Syck;
-        say Dump($res);
+        $DB::single = 1;
         die "Something went wrong:\n$content";
     }
     return $content;
@@ -45,10 +186,10 @@ sub req {
 
 sub sessionise {
     my $params = shift;
-    return unless $sk_methods{$params->{method}};
-    $params->{api_sig} = undef;
+    return unless $methods->{$params->{method}}->{auth};
     $params->{sk} = $sk ||= do {
-        my $res = req("auth.gettoken", api_sig => undef);
+        my $res = req("auth.gettoken");
+
         my ($token) = $res =~ m{<token>(.+)</token>}
             or die "no foundo token: $res";
         say "Sorry about this but could you go over here: "
@@ -56,24 +197,21 @@ sub sessionise {
             .$api_key."&token=".$token;
         say "Hit enter to continue...";
         <STDIN>;
-        my $sess = req("auth.getSession",
-            token => $token,
-            api_sig => undef,
-        );
+
+        my $sess = req("auth.getSession", token => $token);
+
         my ($name) = $sess =~ m{<name>(.+)</name>}
             or die "no name!? $sess";
         my ($key) = $sess =~ m{<key>(.+)</key>}
             or die "no key!? $sess";
-        say "Hello thar $name";
-        symlink($key, $sk_symlink);
+        save_key($key)
         $key
     };
 }
 
 sub sign {
     my $params = shift;
-    return unless exists $params->{api_sig};
-    delete $params->{api_sig};
+    return unless $methods->{$params->{method}}->{signed};
     my $jumble = join "", map { $_ => $params->{$_} } sort keys %$params;
     my $hash = md5_hex($jumble.$secret);
     $params->{api_sig} = $hash;
---
+++ sync-api-docs.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use v5.10;
+use File::Slurp;
+use lib "$ENV{HOME}/pquery-pm/lib";
+use pQuery;
+
+
+my @methods;
+pQuery("http://www.last.fm/api/intro")
+->find("div#leftcol .wspanel ul li")
+->each(sub{
+    sleep 1;
+    $_ = pQuery($_)->html;
+    say "link: $_";
+    m{<a href="(/api/show/\?service=\d+)">(.+)</a>} || die "not <a>: $_";
+    my $method = $2;
+    $_ = pQuery("http://www.last.fm$1")->find("div#wsdescriptor")->html;
+    my $auth = m{This service requires authentication};
+    my $sig = m{<span class="param">api_sig</span>};
+    my $post = m{must be accessed with an HTTP POST request};
+    push @methods, {
+        method => $method,
+        post => $post,
+        signed => $sig,
+        auth => $auth,
+    };
+});
+
+
+my @new;
+my @old = read_file("LastFuckingM.pm");
+push @new, shift @old until $new[-1] =~ /^my \$methods = {/;
+say shift @old until $old[0] =~ /^};/;
+
+for my $m (@methods) {
+    my $attributes = join ", ", map { "$_ => 1" } grep { $m->{$_} } qw{auth post signed};
+    push @new, sprintf("    '%s' => { %s },\n", $m->{method}, $attributes);
+}
+push @new, @old;
+write_file("LastFuckingM.pm", @new);
+
+say "done.";
