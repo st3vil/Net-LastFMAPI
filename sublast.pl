6b5792eb5412e827170759a9c87513a2fa849de3
Steve <nostrasteve@gmail.com>
2011-11-03 02:05:36 +1300

    add submission script

--- sublast.pl
+++ sublast.pl
@@ -1,35 +1,43 @@
 #!/usr/bin/perl
+# submit scrobbles
 use strict;
 use warnings;
 use v5.10;
 use File::Slurp;
 use LastFuckingM;
 
-my $bullytime = "@ARGV" =~ /bullshit-timestamps/;
+my $bullytime = 0 if "@ARGV" =~ /bullshit-timestamps/;
 
 my @log = read_file('../scrobbler.log');
-my $i = 0;
+my @set;
 for (@log) {
     next if /^#/;
     my ($artist, $album, $song, $trackpos, $length, $rating, $ts) = split /\t/;
-    if ($bullytime) {
-        $ts = time - ((@log * 60 * 5) - ($i * 60 * 5));
+    if (defined $bullytime) {
+        $ts = time - ((@log * 60 * 5) - ($bullytime++ * 60 * 5));
     }
-    $i++;
-    say "(".scalar(localtime($ts))." $artist - $song";
-    my $res = LastFuckingM::req(
-        "track.scrobble",
+    say "(".scalar(localtime($ts)).") $artist - $song";
+    push @set, {
         artist => $artist,
         album => $album,
         track => $song,
         timestamp => $ts,
+    };
+    submat() if @set == 50
+}
+submat() if @set;
+
+sub submat {
+    my $res = lastfm(
+        "track.scrobble",
+        @set,
     );
-    unless ($res =~ /accepted="1"/) {
+    my $n = @set;
+    unless ($res =~ /accepted="$n"/) {
         say "For fucks sake: $res";
         say "Consider --bullshit-timestamps" if $res =~ /Timestamp failed/;
         exit;
     }
-    sleep 1;
-};
-
+    @set = ();
+}
 
