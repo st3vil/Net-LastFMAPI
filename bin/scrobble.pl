bce1467f4ae94035b521bd3be5bdfe49dd4118ee
Steve <nostrasteve@gmail.com>
2011-11-13 14:15:39 +1300

    add scrobble.pl for manual scrobbling

---
+++ bin/scrobble.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+# submit one scrobble, pass an argument like: Artist Name - Track Title 
+use strict;
+use warnings;
+use v5.10;
+use lib "../lib";
+use Net::LastFMAPI;
+
+my $track = "@ARGV";
+unless ($track) {
+    say "Enter thy track like Artist - Title";
+    $track = <STDIN>;
+}
+say "...";
+my @track = split /\s*-\s*/, $track;
+my %params;
+$params{track} = pop @track;
+$params{artist} = shift @track;
+$params{album} = shift @track if @track;
+my $res = lastfm(
+    "track.scrobble",
+    %params,
+    timestamp => scalar(time()),
+);
+unless ($res =~ /accepted="1"/) {
+    die "For fucks sake: $res";
+}
+else {
+    say "Good good.";
+}
