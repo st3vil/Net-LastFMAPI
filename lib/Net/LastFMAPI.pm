6ae9fd2188e86fb148ec64926e70dd909fbca2cc
Steve <nostrasteve@gmail.com>
2012-02-07 19:58:45 +1300

    changelog

--- lib/Net/LastFMAPI.pm
+++ lib/Net/LastFMAPI.pm
@@ -14,7 +14,7 @@ use Exporter 'import';
 our @EXPORT = ('lastfm', 'lastfm_config', 'lastfm_iter');
 use Carp;
 
-our $VERSION = "0.62";
+our $VERSION = "0.63";
 our $url = 'http://ws.audioscrobbler.com/2.0/';
 our $api_key = 'dfab9b1c7357c55028c84b9a8fb68880';
 our $secret = 'd004c86dcfa8ef4c3977b04f558535f2';
